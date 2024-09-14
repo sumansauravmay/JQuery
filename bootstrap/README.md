c3697315ed5d5f78877e645d002dc48921e1a256
sumansauravmay <sumansauravmay12345@gmail.com>
2024-09-15 00:47:55 +0530

    react bootstrap

---
+++ bootstrap/README.md
@@ -0,0 +1,6 @@
+1. npx create-react-app app
+2. cd app
+3. npm i bootstrap react-bootstrap
+4. add this in the components
+import {Button} from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
---
+++ bootstrap/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Button>This is button</Button>
+    </div>
+  );
+}
+
+export default App;
